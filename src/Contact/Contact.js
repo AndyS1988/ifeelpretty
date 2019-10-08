36d77503b852b2694d93516333ece83625e7b494
AndyS1988 <spaska.andrea@gmail.com>
2019-10-07 20:45:54 -0700

    modified contact component

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -11,12 +11,21 @@ const Contact = () => {
         <a href="sms:+16194818525?body=Hello Sofia, I would like to see if you are available for " className="title-link">(619) 481-8525</a>         
       </section>
       <section className="container-contact-map">
+{/* Cookie-Less google map??!!       
         <iframe
           id="google-map"
           title="google-map" 
+          src='https://www.uwp.is.ed.ac.uk/3rd-party-widgets/maps/embeds/gm.php?map=6l6wZLtHaB' 
+          frameBorder='0' 
+          className='uwpgmap'>
+        </iframe>*/}
+        <iframe
+          id="google-map"
+          title="google-map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3358.130423807058!2d-117.17971968486479!3d32.682577181000646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80deaccc4ec559dd%3A0x3831ca820affcb01!2s1351%20Orange%20Ave%2C%20Coronado%2C%20CA%2092118!5e0!3m2!1sen!2sus!4v1570315773398!5m2!1sen!2sus" 
           frameBorder="0"
-          allowFullScreen="">
+          allowFullScreen=""
+        >
         </iframe>
       </section>
       <section className="container-contact-details centered-row">
